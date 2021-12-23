2971f6bfa86485e698067e95c39d8aed58cab53d
yusufsolmazz <96589765+yusufsolmazz@users.noreply.github.com>
2021-12-23 20:01:28 +0300

    Add files via upload

---
+++ public/chat.js
@@ -0,0 +1,27 @@
+const socket = io.connect('https://c40b-212-133-204-122.ngrok.io/')
+
+const sender = document.getElementById('sender')
+const message = document.getElementById('message')
+const submitBtn = document.getElementById('submitBtn')
+const output = document.getElementById('output')
+const feedback = document.getElementById('feedback')
+
+submitBtn.addEventListener('click', ()=>{
+    socket.emit('chat', {
+        message: message.value,
+        sender: sender.value
+    })
+})
+
+socket.on('chat', data =>{
+    output.innerHTML += '<p><strong>' + data.sender + ' : </strong>' + data.message + '</p>'
+    message.value = '';
+})
+
+message.addEventListener('keypress', () =>{
+    socket.emit('typing',sender.value)
+})
+
+socket.on('typing', data =>{
+    feedback.innerHTML = '<p>' + data + ' Yaziyor...</p>'
+})
